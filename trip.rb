e6e18d7fbd9e29913a1d70223fbda3e25791a979
KarinaMealey <kem32002@gmail.com>
2018-06-18 20:31:25 +0000

    automatically backed up by learn

--- trip.rb
+++ trip.rb
@@ -12,7 +12,7 @@ puts "Where would you like to eat?"
 eats = gets.chomp
 
 # Do the same thing with `eats` and one last time with `nights`, capturing their text input and storing them to the respective local variable
-puts "How long would you like to stay?"
+puts "How many nights would you like to stay?"
 nights = gets.chomp 
 gets = nights
 
